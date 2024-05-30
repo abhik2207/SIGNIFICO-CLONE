2adf50943844356736ae76b672ce37c609c72a1e
Abhik <abhikgupta01@gmail.com>
2024-05-31 02:33:19 +0530

    Significo

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("HELLO");
+function homepageAnimation() {
+    gsap.set('#marquee-container', {
+        scale: 5
+    });
+
+    var tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#homepage",
+            start: "top top",
+            end: "bottom top",
+            scrub: 3,
+            pin: true
+        }
+    });
+
+    tl
+        .to('#video-container', {
+            '--clip': "0%",
+            ease: Power2,
+        }, 'a')
+        .to('#marquee-container', {
+            scale: 1,
+            ease: Power2,
+        }, 'a')
+        .to('.lft', {
+            xPercent: -25,
+            ease: Power4,
+            stagger: 0.03
+        }, 'b')
+        .to('.rgt', {
+            xPercent: 25,
+            ease: Power4,
+            stagger: 0.03
+        }, 'b');
+}
+homepageAnimation();
